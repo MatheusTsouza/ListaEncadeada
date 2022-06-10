9c6dd7af154f69f94353548e34bcea70d1d81bce
Andrey Silva Silveira <andreysilveira.1997@gmail.com>
2022-06-09 23:38:05 -0300

    Rodou

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,20 @@ public class Main {
     public static void main(String[] args) {
         DoubleLinkedList node = new DoubleLinkedList();
 
-        //node.add(10);
-        //node.add(5);
-        //node.add(2);
 
-        System.out.println(node.getSize());
+        node.add(10);
+        node.add(5);
+        node.add(2);
+        System.out.println("Tamanho: " + node.getSize());
+        node.add(9);
+        node.add(7);
+        node.removeUltimo();
+        node.add(27);
+        System.out.println("Tamanho: " + node.getSize());
+        System.out.println(node.get(0));
+        node.set(1, 99);
+        System.out.println(node);
+        node.remove(3);
+        System.out.println(node);
     }
 }
